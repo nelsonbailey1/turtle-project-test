6d97695d01776d39caafad937d3390706f4a0d79
gliza03 <99229900+gliza03@users.noreply.github.com>
2024-12-02 12:11:00 -0700

    Gabe-1
    Added initial files for deployment

---
+++ index.js
@@ -0,0 +1,21 @@
+let express = require("express");
+let app = express();
+let path = require("path");
+const port = process.env.PORT || 5500;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DB_NAME,
+        port: process.env.RDS_PORT,
+        ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
+    }
+});
+
+app.listen(port, () => console.log("Express App has started and server is listening!"));
