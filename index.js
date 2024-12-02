b0f0587e15193915225d0511b1496ceb3c1f46c8
nelsonbailey1 <137141623+nelsonbailey1@users.noreply.github.com>
2024-12-02 14:04:01 -0700

    Update index.js, deleted knex setup

--- index.js
+++ index.js
@@ -7,18 +7,6 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'));
 
-const knex = require("knex")({
-    client: "pg",
-    connection: {
-        host: process.env.RDS_HOSTNAME,
-        user: process.env.RDS_USERNAME,
-        password: process.env.RDS_PASSWORD,
-        database: process.env.RDS_DB_NAME,
-        port: process.env.RDS_PORT,
-        ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
-    }
-});
-
 app.get("/", (req, res) => res.render("index"));
 
 
