6141167abaa94018818014f8db33116720181c07
Nelson Bailey <nelsonbailey@Nelsons-MBP-3.app.byu.edu>
2024-12-02 13:21:58 -0700

    testing landing page with styles

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5500;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static('public'));
 
 const knex = require("knex")({
     client: "pg",
