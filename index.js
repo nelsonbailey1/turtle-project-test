baa2fc9d70770869d8d83164297b4d96bb104a7f
nelsonbailey1 <137141623+nelsonbailey1@users.noreply.github.com>
2024-12-02 14:14:53 -0700

    Update index.js port for cloudflare

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let app = express();
 let path = require("path");
-const port = process.env.PORT || 5500;
+const port = 8080;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
